1e34b934b19457737cc9fe8359f34ecd70b2b428
ggfdute <david.geho@aol.com>
2021-02-03 17:57:58 -0500

    finished the bank

--- accounts/savings/savings.js
+++ accounts/savings/savings.js
@@ -1,8 +1,36 @@
-var modal = document.getElementById('id01');
+let balance = 0;
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+function add(){
+    let deposit = parseInt(document.getElementById("amount-in").value);
+    
+    if(deposit < 1){
+        document.getElementById("error").innerHTML = "Can not deposie a negative amount or Zero.";
+        document.getElementById("error").style.display = "block";
+        document.getElementById("error").style.color = "red";
+    }else{
+        document.getElementById("error").innerHTML = "Successful";
+        document.getElementById("error").style.display = "block";
+        document.getElementById("error").style.color = "green";
+        balance += deposit;
     }
+    
+}
+
+function subtract(){
+    let withdrawal = parseInt(document.getElementById("amount-out").value);
+    if(withdrawal > balance){
+        document.getElementById("error").innerHTML = "You can not take out more money than you have in.";
+        document.getElementById("error").style.display = "block";
+        document.getElementById("error").style.color = "red";
+    }else{
+        document.getElementById("error").innerHTML = "Successful";
+        document.getElementById("error").style.display = "block";
+        document.getElementById("error").style.color = "green";
+        balance -= withdrawal;
+    }
+}
+
+function money() {
+    alert("Your balance is $" + balance);
+    document.getElementById("error").style.display = "none";
 }
